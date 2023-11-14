3e03b8d4d8d5501ebc46e43b133049267caf58eb
Sebastian Ramirez <u20201c031@upc.edu.pe>
2023-11-13 20:20:54 -0500

    Revert "feat(consulta a tourist con jwt)"
    This reverts commit c91cd7500420203cd3c7fa9de4d281ea47cae96a.

--- services/touristService.js
+++ services/touristService.js
@@ -1,33 +1,24 @@
 const axios = require('axios');
-const jwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJwZXJzZWJhczExQGVtYWlsLmNvbSIsImlhdCI6MTY5OTEzODEyMSwiZXhwIjoxNjk5MTgxMzIxfQ.SW9k6l9Iq-dZ_BZ2QW3fq_0SOX6oMgK-G_GlTkzPAuY'; // Reemplaza con tu token JWT válido
-
-// Configura una instancia de Axios con el token en el encabezado
-const axiosInstance = axios.create({
-  headers: {
-    'Authorization': `Bearer ${jwtToken}` // Incluye el token en el encabezado 'Authorization'
-  }
-});
 
 const getTourists = async () => {
-  try {
-    const response = await axiosInstance.get('http://localhost:3000/tourists');
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching tourists:", error);
-    return null;
-  }
-};
-
+    try {
+        const response = await axios.get('http://localhost:3000/tourists');
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching tourists:", error);
+        return null;
+    };
+}
 const getTouristById = async (touristId) => {
-  try {
-    const response = await axiosInstance.get(`http://localhost:3000/tourists/${touristId}`);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error al obtener el turista con ID ${touristId}: ${error.message}`);
-  }
+        try {
+            const response = await axios.get(`http://localhost:3000/tourists/${touristId}`);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Error al obtener el turista con ID ${touristId}: ${error.message}`);
+        }
 };
 
 module.exports = {
-  getTouristById,
-  getTourists
+    getTouristById,
+    getTourists
 };

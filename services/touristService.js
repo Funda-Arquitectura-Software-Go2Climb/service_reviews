e70c533c118ac0eb9c5dbe76b493249e70ce57eb
Sebastian Ramirez <u20201c031@upc.edu.pe>
2023-11-19 17:08:07 -0500

    Update touristService.js

--- services/touristService.js
+++ services/touristService.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getTourists = async () => {
     try {
-        const response = await axios.get('http://host.docker.internal:3002/tourists');
+        const response = await axios.get('https://touristrepo.onrender.com/tourists');
         return response.data;
     } catch (error) {
         console.error("Error fetching tourists:", error);
@@ -11,7 +11,7 @@ const getTourists = async () => {
 }
 const getTouristById = async (touristId) => {
         try {
-            const response = await axios.get(`http://host.docker.internal:3002/tourists/${touristId}`);
+            const response = await axios.get(`https://touristrepo.onrender.com/tourists/${touristId}`);
             return response.data;
         } catch (error) {
             throw new Error(`Error al obtener el turista con ID ${touristId}: ${error.message}`);
